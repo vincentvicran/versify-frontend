f099176a7af59028c0d37d3d050c0306669e0964
vincentvicran <vvicran@gmail.com>
2022-04-16 16:30:12 +0545

    Basic Framework Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Login from './components/login/login'
+import ChatWindow from "./components/chatWindow/chatWindow";
+import { createSignalProtocolManager, SignalServerStore } from "./signal/SignalGateway"
+
 import './App.css';
+export default class ChatApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoggedIn: false,
+      loggedInUserObj: {},
+      dummySignalServer: new SignalServerStore(),
+      signalProtocolManagerUser: undefined
+    }
+    this.setLoggedinUser = this.setLoggedinUser.bind(this)
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  setLoggedinUser(loggedInUserObj) {
+    this.setState({ isLoggedIn: true, loggedInUserObj: { ...loggedInUserObj } }, () => {
+      // Initializing signal server here
+      createSignalProtocolManager(loggedInUserObj._id, loggedInUserObj.name, this.state.dummySignalServer)
+        .then(signalProtocolManagerUser => {
+          this.setState({ signalProtocolManagerUser: signalProtocolManagerUser })
+        })
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        { !this.state.isLoggedIn && <Login loginProp={this.setLoggedinUser} />}
+        { this.state.isLoggedIn && <ChatWindow
+          loggedInUserObj={this.state.loggedInUserObj}
+          signalProtocolManagerUser={this.state.signalProtocolManagerUser}
+        />}
+      </div>
+    )
+  }
 }
-
-export default App;
